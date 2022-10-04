fbf4ad20d86e2de0445133d21621d51b78653c49
Aspirin 💆🏽 <samuellyworld@gmail.com>
2022-10-04 16:03:23 +0100

    🎉 navigations done

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-
-
+import Navbar from "./components/navbar/navbar";
 function App() {
   return (
     <div className="App">
-       
+      <Navbar />
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './styles/global.scss';
 import  App from './App';
 
 
