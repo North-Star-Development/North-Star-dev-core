33562030eed6cba052387e21540991ff783e3929
Aspirin 💆🏽 <samuellyworld@gmail.com>
2022-10-04 17:31:24 +0100

    🗺adding splash section

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+//components
 import Navbar from "./components/navbar/navbar";
+import SplashSection from "./components/splash-section/splash-section";
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <SplashSection />
     </div>
   );
 }
--- src/components/splash-section/splash-section.tsx
+++ src/components/splash-section/splash-section.tsx
@@ -1,17 +1,22 @@
 import React from "react";
 import Button from "../button/button";
+
+const splashImg = require('../../assets/png/splash-img.png');
+//resoure
 const SplashSection = () => {
    return (
      <section className="splash">
          <div className="splash__section1">
              <p>Welcome to NorthStar</p>
              <h2>Software development at the speed of light</h2>
-             <h3> Northstar is a design and software development agency that delivers affordable, professional, and fast service. We've got your back!</h3>
+             <p> Northstar is a design and software development agency that delivers affordable, professional, and fast service. We've got your back!</p>
              <div className="splash__section1-buttons">
                <Button text="Get in Touch" />
                <Button text="Learn More" />
              </div>
          </div>
+         
+         <img src={splashImg} alt="splash section" /> 
      </section>
    );
 }
