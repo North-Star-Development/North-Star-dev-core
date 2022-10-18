61752e19bdc36de7144f4cfab29c309301a40f6d
Aspirin 💆🏽 <samuellyworld@gmail.com>
2022-10-19 00:48:04 +0100

    done withmobile navbar 𝌼

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -14,7 +14,7 @@ const northStarLogo = require('../../assets/png/northstar.png');
 //JSX Component
 const Navbar = () => {
   const width = useWindowWidth();
-  const [toggleMenu, setToggleMenu] = useState(false);
+  const [toggleMenu, setToggleMenu]:any = useState(false);
 
     return (
       <nav className='nav'>
@@ -33,12 +33,37 @@ const Navbar = () => {
             </div>
             <Button primary={true} text='Get in Touch' />
           </> : 
-
-            <div className={`menu-bars ${toggleMenu && "change" }`} id="menu-bars" onClick={() => setToggleMenu(!toggleMenu)}>
-                  <div className="bar1"  ></div>
-                  {/* <div className="bar2"></div> */}
-                  <div className="bar3"></div>
+            <> 
+             <div  style={{
+               background: toggleMenu && "unset",
+               color :  toggleMenu && "#2851E3"
+             }}
+             className={`menu-bars ${toggleMenu && "change" }`} 
+              id="menu-bars" 
+              onClick={() => setToggleMenu(!toggleMenu)}>
+              <div className="bar1" style={{
+                background :  toggleMenu && "#2851E3"
+              }} ></div>
+                {/* <div className="bar2"></div> */}
+              <div className="bar3" style={{
+                background :  toggleMenu && "#2851E3"
+              }}></div>
             </div>
+            <div className={`nav-overlay ${toggleMenu ? "overlay-slide-right": "overlay-slide-left"}`} id="nav-overlay" >
+             <div className="nav-bar"> 
+            <ul>
+                <li onClick={() => setToggleMenu(!toggleMenu)} id="nav-1" className={`${toggleMenu ? "slide-in-1" : "slide-out-1"} `}><a href="#home">Home</a></li>
+                <li onClick={() => setToggleMenu(!toggleMenu)} id="nav-2" className={`${toggleMenu ? "slide-in-2" : "slide-out-2"} `}><a href="#about">About</a></li>
+                <li onClick={() => setToggleMenu(!toggleMenu)} id="nav-3" className={`${toggleMenu ? "slide-in-3" : "slide-out-3"} `}><a href="#skills"  >Tools & Technologies</a></li>
+                <li onClick={() => setToggleMenu(!toggleMenu)} id="nav-4" className={`${toggleMenu ? "slide-in-4" : "slide-out-4"} `}><a href="#projects">Projects</a></li>
+                <li onClick={() => setToggleMenu(!toggleMenu)} id="nav-5" className={`${toggleMenu ? "slide-in-5" : "slide-out-5"} `}><a href="#contact">Contact</a></li>
+                <li onClick={() => setToggleMenu(!toggleMenu)} id="nav-6" className={`${toggleMenu ? "slide-in-6" : "slide-out-6"} `}><a href="#contact">Contact</a></li>
+                <li onClick={() => setToggleMenu(!toggleMenu)} id="nav-7" className={`${toggleMenu ? "slide-in-7" : "slide-out-7"} `}><a href="#contact">Contact</a></li>
+              </ul>
+         </div>
+             </div>
+            </>
+      
              
           }
       </nav>
